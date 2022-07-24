4a4c962d6c3cf94ffc802b4f8efc464daff1b583
Elisha Mallard <emallard@EXI231121-01.ad.exigentcap.com>
2022-07-24 19:32:21 +0300

    auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,55 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-        <div>
-          <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
+    <div className="stick top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      <div className="flex items-center m-4">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 rounded-full bg-gray-100 p-2">
+        <SearchIcon className="h-6 text-gray-600"  />
+        <input className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
+      </div>
+      </div>
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
         </div>
+      </div>
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* <Image /> */}
+        <p className=" whitespace-nowrap font-semibold pr-3">Sonny Sangha</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ session }: any ) => {
+  if(!session) return <Login />
   return (
     <div className="">
       <Head>
@@ -16,6 +19,17 @@ const Home: NextPage = () => {
     </main>
       </div>
   )
+}
+
+export async function getServerSideProps(context: any) {
+  
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
 
 export default Home
